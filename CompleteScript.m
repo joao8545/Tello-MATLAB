c1f0a965d2a5ebf822e9cf3765f6ec38280d03fe
Joao Anastacio <almeida.jar97@gmail.com>
2023-03-13 20:38:59 -0300

    Last changes

--- CompleteScript.m
+++ CompleteScript.m
@@ -4,14 +4,18 @@ clc;
 
 droneObj=ryze()
 cameraObj = camera(droneObj)
+takeoff(droneObj);
 
 data=load("Calibration\Test-6\cameraParams.mat");
 intrinsics=data.cameraParams.Intrinsics;
 tagSize=0.08;
 tagFamily=["tag36h11",];
+vid=VideoWriter("Output\Test0");
+open(vid);
 
 while true
     I=snapshot(cameraObj);
+    writeVideo(vid,I);
     I=undistortImage(I,intrinsics,OutputView="same");
     [id,loc,pose]=readAprilTag(I,tagFamily,intrinsics,tagSize);
     worldPoints=[0 0 0; tagSize/2 0 0; 0 tagSize/2 0; 0 0 tagSize/2];
@@ -25,12 +29,25 @@ while true
             Color=["red","green","blue"],LineWidth=7);
 
         I = insertText(I,loc(1,:,i),id(i),BoxOpacity=1,FontSize=25);
-        disp("The target is "+pose(i).Translation(3)+" cm away, "+pose(i).Translation(1)+" cm to the left and "+pose(i).Translation(2)+" cm downwards")
+        target = pose(i);
+        disp("The target (id="+id(i)+ ") is "+pose(i).Translation(3)+" cm away, "+pose(i).Translation(1)+" cm to the left and "+pose(i).Translation(2)+" cm downwards")
     end
     %imshow(I);
-    break;
-end
+    targetPos=[target.Translation(3)-0.10, target.Translation(1), target.Translation(2)];
+    targetPos=normalize(targetPos,"range").*0.21;
+    if isempty(id) 
+        disp("No Tag")
+        %land(droneObj);
+        close(vid);
+        break;
+    else
+        targetRot=[0 1 0]*target.R;
+        %targetRot= rotm2eul(target.R);
+        turn(droneObj,targetRot(2)*pi*(-1));
+        break;
+        moveforward(droneObj,duration=0.2);
+        %move(droneObj,targetPos,Speed=0.5,WaitUntilDone=false)
+    end
 
-takeoff(droneObj);
-move(droneObj,[pose(3)-10, pose(1), pose(2)],Speed=0.5)
+end
 
